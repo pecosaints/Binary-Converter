8e8f2a31e89059344db30344556aa1d813f0fa54
Pedro Guimarães Santos <80483432+pecosaints@users.noreply.github.com>
2021-06-20 19:57:06 -0300

    JavaScript Att
    IF for up to 8 number, else alert user

--- app.js
+++ app.js
@@ -10,31 +10,37 @@ convertBtn.addEventListener("click", function(){
   function binaryToDecimal(val) {
     let sum = 0;
     let i = 0;
-    while (i < val.length) {
+    if (num.length <= 8){
+      while (i < val.length) {
+          //if num is 100:
+          //first iteration: 3 - 0 - 1 = position 2 = 0
+          //second iteration: 3 - 1 - 1 = position 1 = 0
+          //third iteration: 3 - 2 - 1 = position 0 = 1
+        let bit = val.charAt(val.length - i - 1);
+        if (parseInt(bit) == 0 || parseInt(bit) == 1) 
+        {
+        sum += parseInt(bit) * Math.pow(2, i);
         //if num is 100:
-        //first iteration: 3 - 0 - 1 = position 2 = 0
-        //second iteration: 3 - 1 - 1 = position 1 = 0
-        //third iteration: 3 - 2 - 1 = position 0 = 1
-      let bit = val.charAt(val.length - i - 1);
-      if (parseInt(bit) == 0 || parseInt(bit) == 1) 
-      {
-       sum += parseInt(bit) * Math.pow(2, i);
-      //if num is 100:
-      //0 * 2 ** 0 = 0
-      //0 * 2 ** 1 = 0
-      // 1 * 2 ** 2 = 4
+        //0 * 2 ** 0 = 0
+        //0 * 2 ** 1 = 0
+        // 1 * 2 ** 2 = 4
+        }
+        else
+        {
+          alert("Input a valid Number");
+          sum = "";
+          BinNunInpt.value = "";
+          break;
+        }
+        i++;
       }
-      else
-      {
-        alert("Input a valid Number");
-        sum = "";
-        BinNunInpt.value = "";
-        break;
-      }
-      i++;
+      return sum;
+      //If number length is more then 8 digits.
+    }else{
+      alert("Input a number up to 8 binary digits.")
     }
-    return sum;
   }
+
   displayInput.value = binaryToDecimal(num);
 })
 
